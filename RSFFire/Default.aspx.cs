5cbdbb2d058166e8daa082b05d9b44a5569f17b4
Chuck Hedrick <chuck.hedrick@gmail.com>
2015-02-10 13:07:25 -0800

    Working Project List

---
+++ RSFFire/Default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using RSFFireBiz;
+using RSFFireData.Models;
+using System;
+using System.Collections.Generic;
+using System.Web.UI;
+
+namespace RSFFire
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public List<Project> GetProjects(int startRowIndex, int maximumRows, out int totalRowCount)
+        {
+            return RsfFireBusinessLogic.GetProjectListItems(startRowIndex, maximumRows, out totalRowCount);
+        }
+    }
+}
---
+++ RSFFireBiz/RSFFireBusinessLogic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RSFFireData;
+using RSFFireData.Models;
+
+namespace RSFFireBiz
+{
+    public class RsfFireBusinessLogic
+    {
+        public static List<Project> GetProjectListItems(int startRowIndex, 
+                        int maximumRows, out int totalRowCount)
+        {
+            using (var ctx = new RSFFireEntities())
+            {
+                totalRowCount = ctx.Projects.Count();
+                return ctx.Projects.ToList();
+            }
+        }
+    }
+}
